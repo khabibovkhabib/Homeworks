90b104e5680122340bf0069fcc443174bb6ecf6a
khabibovkhabib <habib.habiblee@gmail.com>
2023-06-19 12:35:34 +0400

    Uptadedd

--- Task3js/main.js
+++ Task3js/main.js
@@ -2,10 +2,13 @@ let btn = document.getElementById('btn');
 btn.addEventListener('click', () => {
     document.body.style.backgroundColor = changeC()
 });
+
 function changeC() { 
 let x = Math.floor(Math.random() * 256);
     y = Math.floor(Math.random() * 256);
     z = Math.floor(Math.random() * 256);
-    return  "rgb(" + x + "," + y + "," + z + ")";
+  
+    return  `rgb(${x},${y} ,${z} )`;
 };
+
 

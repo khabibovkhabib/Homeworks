465ca051f37cf19ea9511e963b977119d0bd2de9
khabibovkhabib <habib.habiblee@gmail.com>
2023-06-19 13:00:18 +0400

    Task2ended

--- Task3js/main.js
+++ Task3js/main.js
@@ -3,12 +3,19 @@ btn.addEventListener('click', () => {
     document.body.style.backgroundColor = changeC()
 });
 
-function changeC() { 
-let x = Math.floor(Math.random() * 256);
+function changeC() {
+    let x = Math.floor(Math.random() * 256);
     y = Math.floor(Math.random() * 256);
     z = Math.floor(Math.random() * 256);
-  
-    return  `rgb(${x},${y} ,${z} )`;
+
+    return `rgb(${x},${y},${z} )`;
+};
+const counterElem = document.querySelector("#counter");
+const but = document.getElementById('btn2');
+let count = 0;
+but.onclick = function incrementCounter(event) {
+    count = count + 1;
+    counterElem.innerHTML = count;
 };
 
 

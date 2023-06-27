9ecaf1ef6b5c608d818b2bffdb6e6f2712118852
khabibovkhabib <habib.habiblee@gmail.com>
2023-06-27 17:54:41 +0400

    firsttaskended

--- Task4js/slider/main.js
+++ Task4js/slider/main.js
@@ -1,19 +1,42 @@
-var slides = document.getElementsByClassName("mySlides");
-switchSlides();
-function switchSlides() {
-  var slideCount = slides.length;
-  var i = 0;
-  slides[i].style.display = "block";
-  var switchInterval = setInterval(function() {
-    i++;
-    if (i < slideCount) {
-      slides[i].style.display = "block";
-      slides[i-1].style.display = "none";
-    } else {
-      clearInterval(switchInterval);
-      slides[0].style.display = "block";
-      slides[i-1].style.display = "none";
-      setTimeout(switchSlides, 100);
+let imageArr = [
+  "image/1.jpg",
+  "image/2.jpg",
+  "image/3.jpg",
+  "image/4.jpg",
+  "image/5.jpg"
+]
+let index = 0;
+let imageCount = imageArr.length;
+let settings = {
+  duration: '1200',
+  random: true
+}
+
+init(settings);
+document.querySelector('.fa-chevron-left').addEventListener('click', function () {
+  index--;
+  showSlide(index);
+});
+document.querySelector('.fa-chevron-right').addEventListener('click', function () {
+  index++;
+  showSlide(index);
+})
+
+function init(settings) {
+  setInterval(function () {
+    if (settings.random) {
+      index= index+1
     }
-  }, 1800);
+    showSlide(index);
+  }, settings.duration)
+}
+
+function showSlide(i) {
+  index = i;
+  if (i < 0) {
+    index = imageCount - 1;
+  } else if (i >= imageCount) {
+    index = 0;
+  }
+  document.querySelector('.mainImage').setAttribute('src', imageArr[index]);
 }
